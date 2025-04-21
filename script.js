87a2a4990a583730f9ef6a9c042cdb8c24cfc8bc
vrexitus <98414540+vrexitus@users.noreply.github.com>
2025-04-21 16:12:41 +0100

    lol

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+const playButton = document.querySelector("#play");
+const score = document.querySelector("#score");
+const rButton = document.querySelector("#rock");
+const pButton = document.querySelector("#paper");
+const sButton = document.querySelector("#scissor");
+const buttonsContainer = document.querySelector(".buttonsContainer");
+const playButtonContainer = document.querySelector(".playButton");
+const humanScore = document.querySelector("#humanScore");
+const computerScore = document.querySelector("#computerScore");
+const restartButton = document.querySelector("#restart");
+const computerChoiceDisplay = document.querySelector("#choice");
+
+restartButton.addEventListener("click", () => {
+    restartGame();
+})
+
+playButton.addEventListener("click", () => {
+    buttonsContainer.style.display = "flex";
+    playButtonContainer.style.display = "none";
+    startGame();
+});
+
+rButton.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+});
+pButton.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+});
+sButton.addEventListener("click", () => {
+    playRound("scissor", getComputerChoice());
+});
+
+let humanButtonScore = 0;
+let computerButtonScore = 0;
+let rounds = 0;
+
 function getComputerChoice(){
     let max = 3;
     let min = 1;
@@ -5,93 +41,80 @@ function getComputerChoice(){
     let choice = "";
     if (numChoice === 1){
         choice = "rock";
+        computerChoiceDisplay.textContent = `Computer chose: ${choice}`;
     } else if (numChoice === 2) {
         choice = "paper";
+        computerChoiceDisplay.textContent = `Computer chose: ${choice}`;
     } else if (numChoice === 3) {
         choice = "scissor";
+        computerChoiceDisplay.textContent = `Computer chose: ${choice}`;
     } else {
         console.log("ERROR!");
     }
     return String(choice).toLowerCase();
 }
 
-function getHumanChoice(){
-    let choice = String(prompt("What's your move?: ")).toLowerCase();
-    if ((choice != "rock") && (choice != "paper") && (choice != "scissor")){
-        console.log(`Your options are only either "Rock", "Paper" or "Scissor".`);
-        return ;
-    } else {
-        return choice;
-    }
-}
-
 function playRound(choice, computerChoice){
     if (choice == computerChoice){
         return "NONE";
     } else if (choice == "paper" && computerChoice == "rock"){
+        updateScore("HUMAN");
         return "HUMAN";
     } else if (choice == "paper" && computerChoice == "scissor"){
+        updateScore("COMPUTER");
         return "COMPUTER";
     } else if (choice == "rock" && computerChoice == "paper"){
+        updateScore("COMPUTER");
         return "COMPUTER";
     } else if (choice == "rock" && computerChoice == "scissor"){
+        updateScore("HUMAN");
         return "HUMAN";
     } else if (choice == "scissor" && computerChoice == "paper"){
+        updateScore("HUMAN");
         return "HUMAN";
     } else if (choice == "scissor" && computerChoice == "rock"){
+        updateScore("COMPUTER");
         return "COMPUTER";
     } else {
         return "ERROR";
     }
 }
 
-function playGame(rounds){
-    let humanScore = 0;
-    let computerScore = 0;
-    let humanChoice = "";
-    let computerChoice = "";
-    let winner = "";
-    let gameWinner = "";
-    while ((humanScore < rounds) && (computerScore < rounds)){
-        humanChoice = getHumanChoice();
-        while (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissor"){
-            humanChoice = getHumanChoice();
+function updateScore(winner){
+    if (winner == "HUMAN") {
+        humanButtonScore++;
+        humanScore.textContent = `HUMAN: ${humanButtonScore}`;
+        if (humanButtonScore == rounds){
+            computerChoiceDisplay.textContent = `The Winner is: PLAYER!`;
+            buttonsContainer.style.display = "none";
+            restartButton.style.display = "block";
         }
-        computerChoice = getComputerChoice();
-        winner = playRound(humanChoice, computerChoice);
-        if (winner == "NONE") {
-            humanScore += 0;
-            computerScore += 0;
-            alert(`Human chose: ${humanChoice}`);
-            alert(`Computer chose: ${computerChoice}`);
-            alert("Draw!");
-        } else if (winner == "HUMAN") {
-            humanScore += 1;
-            alert(`Human chose: ${humanChoice}`);
-            alert(`Computer chose: ${computerChoice}`);
-            alert("Human wins this round!");
-        } else if (winner == "COMPUTER") {
-            computerScore += 1;
-            alert(`Human chose: ${humanChoice}`);
-            alert(`Computer chose: ${computerChoice}`);
-            alert("Computer wins this round!");
-        } else {
-            alert(`Human chose: ${humanChoice}`);
-            alert(`Computer chose: ${computerChoice}`);
-            alert("ERROR");
-            return ;
+    } else if (winner == "COMPUTER") {
+        computerButtonScore++;
+        computerScore.textContent = `COMPUTER: ${computerButtonScore}`;
+        if (computerButtonScore == rounds) {
+            computerChoiceDisplay.textContent = `The Winner is: COMPUTER!`;
+            buttonsContainer.style.display = "none";
+            restartButton.style.display = "block";
         }
+    } else if (winner == "RESTART") {
+        computerButtonScore = 0;
+        humanButtonScore = 0;
+        computerScore.textContent = `COMPUTER: ${computerButtonScore}`;
+        humanScore.textContent = `HUMAN: ${humanButtonScore}`;
+        computerChoiceDisplay.textContent = `SCORE`;
     }
-    if (humanScore > computerScore) {gameWinner = "Human"} else {gameWinner = "Computer"};
-    alert(`Computer score: ${computerScore}\nHuman score: ${humanScore}\nThe winner is: ${gameWinner}`);
 }
 
 function startGame(){
-    let rounds = parseInt(prompt("How many points to win?: "));
+    rounds = parseInt(prompt("How many points to win?: "));
     while (isNaN(rounds)){
         rounds = parseInt(prompt("How many points to win?: "));
     }
-    playGame(rounds);
 }
 
-startGame();
+function restartGame(){
+    updateScore("RESTART");
+    playButtonContainer.style.display = "flex";
+    restartButton.style.display = "none";
+}
